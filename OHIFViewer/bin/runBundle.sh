37f407e556e449da7cb59c7c0cd043f7d2cffa56
shenkun <shenkun@hotmail.com>
2018-03-09 16:00:16 +0800

    test github

--- OHIFViewer/bin/runBundle.sh
+++ OHIFViewer/bin/runBundle.sh
@@ -3,3 +3,4 @@ export METEOR_SETTINGS=$(cat iCloudArcDICOMWeb.json)
 export MONGO_URL='mongodb://localhost:27017/ohif-viewer'
 export ROOT_URL='http://localhost:3004'
 echo ${METEOR_SETTINGS} & node main.js
+#twst
