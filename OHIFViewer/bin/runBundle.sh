4bf3fc7f159948b5378b30c3980839abb56e3f54
shenkun <shenkun@hotmail.com>
2018-03-09 17:05:15 +0800

    dimse方式影像存储的AE配置在peers[0]的位置。

--- OHIFViewer/bin/runBundle.sh
+++ OHIFViewer/bin/runBundle.sh
@@ -3,4 +3,3 @@ export METEOR_SETTINGS=$(cat iCloudArcDICOMWeb.json)
 export MONGO_URL='mongodb://localhost:27017/ohif-viewer'
 export ROOT_URL='http://localhost:3004'
 echo ${METEOR_SETTINGS} & node main.js
-#twst
